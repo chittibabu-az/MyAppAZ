745d59880397a927536590bb98276f2961d33935
techmachine <techmachine@localhost>
2015-01-23 09:38:08 +0530

    Initial Import : Fetch address details based on IP.

--- GEOLocation/src/main/java/com/mycompany/geolocation/GeoLocatorImpl.java
+++ GEOLocation/src/main/java/com/mycompany/geolocation/GeoLocatorImpl.java
@@ -181,23 +181,7 @@ public class GeoLocatorImpl implements GeoLocator {
         //TODO: 503 Error.
         return returnValue;
     }
-    
-    /**
-     * 
-     * @param hostName
-     * @return
-     * @throws NamingException 
-     */
-//    static int doLookup( String hostName ) throws NamingException {
-//        Hashtable env = new Hashtable();
-//        env.put("java.naming.factory.initial","com.sun.jndi.dns.DnsContextFactory");
-//        DirContext ictx = new InitialDirContext( env );
-//        Attributes attrs = ictx.getAttributes( hostName, new String[] { "MX" });
-//        Attribute attr = attrs.get( "MX" );
-//        if( attr == null ) return( 0 );
-//        return( attr.size() );
-//    }
-    
+   
     /**
      * Get or Open URL Connection.
      * 
